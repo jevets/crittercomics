6c7cd2acff644d94c0ca8951ec16e0056b865884
stevejamesson <steve@evendev.com>
2019-04-20 11:29:46 -0400

    misc styles

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = () => ({
   title: 'Critter Comics',
   author: 'Michelle Jamesson',
   authorUrl: 'https://www.michellejamesson.com/',
-  baseUrl: 'https://www.crittercomics.com/',
+  baseUrl: 'https://www.crittercomics.com',
 
   copyrightDate: new Date().getFullYear(),
 })
